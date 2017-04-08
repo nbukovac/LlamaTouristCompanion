eff4cb8e122e949bad3f14319b6d777b482c0f5c
Brigita <vrbanec.brigita@gmail.com>
2017-04-08 14:34:50 +0200

    Repo interface

---
+++ src/LlamasTouristCompanion/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LlamasTouristCompanion.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get<TKey>(TKey id);
+        IQueryable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
