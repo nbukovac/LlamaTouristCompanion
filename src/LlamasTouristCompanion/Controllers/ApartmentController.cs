2408227173b751f75eaf3368462a45a51e4e617f
Nikola Bukovac <nikola.bukovac@outlook.com>
2017-04-09 02:04:47 +0200

    ApartmentController finalized

--- src/LlamasTouristCompanion/Controllers/ApartmentController.cs
+++ src/LlamasTouristCompanion/Controllers/ApartmentController.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LlamasTouristCompanion.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using LlamasTouristCompanion.Models;
 using LlamasTouristCompanion.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LlamasTouristCompanion.Controllers
 {
+    [Authorize]
     public class ApartmentController : Controller
     {
 
@@ -22,6 +22,7 @@ namespace LlamasTouristCompanion.Controllers
         {
             _apartmentService = apartmentService;
             _ownerService = ownerService;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -38,10 +39,36 @@ namespace LlamasTouristCompanion.Controllers
                 var user = await GetActiveUserId();
                 var owner = await _ownerService.GetOwnerByUserIdAsync(user.ToString());
                 await _apartmentService.AddAsync(model, owner.OwnerId.ToString());
-                return RedirectToAction("Index");
+
+                return RedirectToAction("Index", "Owner");
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            return View(_apartmentService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                _apartmentService.Update(apartment);
+                return RedirectToAction("Index", "Owner");
+            }
+
+            return View(apartment);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            _apartmentService.Delete(id);
+            return RedirectToAction("Index", "Owner");
         }
 
         private async Task<Guid> GetActiveUserId()
